c428c657ba1dded4d408d097afb9ddc88914d5fa
duonghoanglong0410 <95846918+duonghoanglong0410@users.noreply.github.com>
2024-09-14 01:11:34 +0700

    Add Controller HelloWorld

---
+++ Controllers/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce_ThucTap.Controllers
+{
+	public class HelloWorld : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
