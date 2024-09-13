c42fb85a028e2a3ee5ffeeea7b094c2ad6e84b03
duonghoanglong0410 <95846918+duonghoanglong0410@users.noreply.github.com>
2024-09-14 00:57:25 +0700

    Intial comit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ECommerce_ThucTap.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
